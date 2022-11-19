9e4e1b7a2638c798d5572d3e0ff931eda0740253
tomas-fryza <48966045+tomas-fryza@users.noreply.github.com>
2022-11-19 12:44:12 +0100

    Update main.c

--- examples/gpio/src/main.c
+++ examples/gpio/src/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>              // Needed for "printf" function
-#include <esp_log.h>            // ESP_LOGE/W/I functions
+#include <esp_log.h>            // ESP_LOG/E/W/I functions
 #include <freertos/FreeRTOS.h>  // FreeRTOS
 #include <freertos/task.h>      // vTaskDelay, portTICK_PERIOD_MS
 #include <driver/gpio.h>        // GPIO pins
@@ -7,31 +7,32 @@
 // ESP32-CAM on-board LEDs: #33 (red, bottom side), #4 (Flash, top side)
 #define BUILT_IN_LED 33
 
-// In ESP-IDF instead of "main", we use "app_main" function
-// where the program execution begins
+/* In ESP-IDF instead of "main", we use "app_main" function
+   where the program execution begins */
 void app_main()
 {
-    // Pin(s) configuration:
-    gpio_pad_select_gpio(BUILT_IN_LED);
-    gpio_set_direction(BUILT_IN_LED, GPIO_MODE_OUTPUT);
-
     // Standard output to serial monitor
-    printf("\nHi, there !\n");
-    printf("\033[1;31mBold, red text by ANSI Escape sequences\033[0m\n");
+    printf("\nHi, there!\n");
 
     // Advanced logging functionality for three main levels (Error, Warning, Info)
-    uint16_t log_level = 1;
-    ESP_LOGE("setup", "This is Error Logging with level %d", log_level++);
-    ESP_LOGW("setup", "This is Warning Logging with level %d", log_level++);
-    ESP_LOGI("setup", "This is Info Logging with level %d\n", log_level++);
+    ESP_LOGE("setup", "this is Error Logging");
+    ESP_LOGW("setup", "this is Warning Logging");
+    ESP_LOGI("setup", "this is Info Logging");
 
-    while (1) {
-        gpio_set_level(BUILT_IN_LED, 1);
-        ESP_LOGI("gpio", "LED #%d is off", BUILT_IN_LED);
+    // Pin(s) configuration
+    gpio_pad_select_gpio(BUILT_IN_LED);
+    gpio_set_direction(BUILT_IN_LED, GPIO_MODE_OUTPUT);
+    ESP_LOGI("gpio", "pin #%d configured as output", BUILT_IN_LED);
+
+    // Forever loop
+    while (1)
+    {
+        gpio_set_level(BUILT_IN_LED, 1);        // Set high level
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // Delay 1 second
 
-        gpio_set_level(BUILT_IN_LED, 0);
-        ESP_LOGW("gpio", "LED #%d is on", BUILT_IN_LED);
+        gpio_set_level(BUILT_IN_LED, 0);        // Set low level
         vTaskDelay(1000 / portTICK_PERIOD_MS);  // Delay 1 second
     }
+
+    // Will never reach this
 }
