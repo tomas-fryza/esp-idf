0cc54dbf117a68d679a8d76679a080502386af8f
Tomas Fryza <tomas.fryza@gmail.com>
2022-12-10 17:54:13 +0100

    Application tested on FireBeetle boards

--- examples/wifi_get_requests/src/main.c
+++ examples/wifi_get_requests/src/main.c
@@ -33,9 +33,13 @@
 
 
 /*-----------------------------------------------------------*/
+static const char *TAG = "wifi station";
+
+
+/*-----------------------------------------------------------*/
 // Used function(s)
 void wifi_connect();
-void wifi_event_handler(void *, esp_event_base_t, int32_t, void *);
+void event_handler(void *, esp_event_base_t, int32_t, void *);
 void vTaskHttpRequest();
 esp_err_t http_event_handler(esp_http_client_event_handle_t);
 
@@ -45,13 +49,19 @@ esp_err_t http_event_handler(esp_http_client_event_handle_t);
    where the program execution begins */
 void app_main(void)
 {
-    nvs_flash_init();
-    esp_netif_init();
-    esp_event_loop_create_default();
+    // Initialize NVS
+    // nvs_flash_init()
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
 
+    ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
     // Establish Wi-Fi connection
     wifi_connect();
-    ESP_LOGI("http_client", "Connected to AP, begin http example");
+    ESP_LOGI(TAG, "Connected to AP, begin http example");
 
     // Create Wi-Fi task
     xTaskCreate(vTaskHttpRequest, "http_get_task", 4096, NULL, 5, NULL);
@@ -62,25 +72,49 @@ void app_main(void)
 void wifi_connect()
 {
     // 1 - Wi-Fi/LwIP Init Phase
-    // esp_netif_init();                  // TCP/IP initiation    s1.1
-    // esp_event_loop_create_default();   // event loop           s1.2
-    esp_netif_create_default_wifi_sta();  // WiFi station         s1.3
-    wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&wifi_initiation);      //                      s1.4
+    ESP_ERROR_CHECK(esp_netif_init());    // TCP/IP initiation     s1.1
+    ESP_ERROR_CHECK(esp_event_loop_create_default());// event loop s1.2
+
+    esp_netif_create_default_wifi_sta();  // WiFi station          s1.3
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    esp_wifi_init(&cfg);                  //                       s1.4
 
     // 2 - Wi-Fi Configuration Phase
-    esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
-    esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
-    wifi_config_t wifi_configuration = {
+    // esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
+    // esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(
+        esp_event_handler_instance_register(
+            WIFI_EVENT,
+            ESP_EVENT_ANY_ID,
+            &event_handler,
+            NULL,
+            &instance_any_id));
+    ESP_ERROR_CHECK(
+        esp_event_handler_instance_register(
+            IP_EVENT,
+            IP_EVENT_STA_GOT_IP,
+            &event_handler,
+            NULL,
+            &instance_got_ip));
+
+    wifi_config_t wifi_config = {
         .sta = {
             .ssid = SSID,
-            .password = PASS
+            .password = PASS,
+            // .threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,
+            // .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
         }
     };
-    esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    // esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config);
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
 
     // 3 - Wi-Fi Start Phase
     esp_wifi_start();
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
 
     // 4- Wi-Fi Connect Phase
     esp_wifi_connect();
@@ -91,20 +125,20 @@ void wifi_connect()
 
 
 /*-----------------------------------------------------------*/
-void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+void event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
 {
     switch (event_id) {
         case WIFI_EVENT_STA_START:
-            ESP_LOGI("wifi_event_handler", "WiFi STA connecting...");
+            ESP_LOGI(TAG, "WiFi STA connecting...");
             break;
         case WIFI_EVENT_STA_CONNECTED:
-            ESP_LOGI("wifi_event_handler", "WiFi connected...");
+            ESP_LOGI(TAG, "WiFi connected...");
             break;
         case WIFI_EVENT_STA_DISCONNECTED:
-            ESP_LOGW("wifi_event_handler", "WiFi lost connection");
+            ESP_LOGW(TAG, "WiFi lost connection");
             break;
         case IP_EVENT_STA_GOT_IP:
-            ESP_LOGI("wifi_event_handler", "WiFi got IP...");
+            ESP_LOGI(TAG, "WiFi got IP...");
             break;
         default:
             break;
@@ -115,7 +149,7 @@ void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_base, in
 /*-----------------------------------------------------------*/
 void vTaskHttpRequest()
 {
-    ESP_LOGI("http_request", "Start http_request example");
+    ESP_LOGI(TAG, "Start http_request example");
 
     esp_http_client_config_t config_get_request = {
         .url = "http://worldclockapi.com/api/json/utc/now",
@@ -135,7 +169,7 @@ void vTaskHttpRequest()
     }
 
     // Delete this task if it exits from the loop above
-    ESP_LOGI("http_request", "Finish http_request example");
+    ESP_LOGI(TAG, "Finish http_request example");
     vTaskDelete(NULL);
 }
 
@@ -145,25 +179,25 @@ esp_err_t http_event_handler(esp_http_client_event_handle_t evt)
 {
     switch (evt->event_id) {
         case HTTP_EVENT_ERROR:
-            ESP_LOGW("http_client", "HTTP_EVENT_ERROR");
+            ESP_LOGW(TAG, "HTTP_EVENT_ERROR");
             break;
         case HTTP_EVENT_ON_CONNECTED:
-            ESP_LOGI("http_client", "HTTP_EVENT_ON_CONNECTED");
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_CONNECTED");
             break;
         case HTTP_EVENT_HEADER_SENT:
-            ESP_LOGI("http_client", "HTTP_EVENT_HEADER_SENT");
+            ESP_LOGI(TAG, "HTTP_EVENT_HEADER_SENT");
             break;
         case HTTP_EVENT_ON_HEADER:
-            ESP_LOGI("http_client", "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_HEADER, key=%s, value=%s", evt->header_key, evt->header_value);
             break;
         case HTTP_EVENT_ON_DATA:
-            ESP_LOGI("http_client", "HTTP_EVENT_ON_DATA: %.*s", evt->data_len, (char *)evt->data);
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_DATA: %.*s", evt->data_len, (char *)evt->data);
             break;
         case HTTP_EVENT_ON_FINISH:
-            ESP_LOGI("http_client", "HTTP_EVENT_ON_FINISH");
+            ESP_LOGI(TAG, "HTTP_EVENT_ON_FINISH");
             break;
         case HTTP_EVENT_DISCONNECTED:
-            ESP_LOGI("http_client", "HTTP_EVENT_DISCONNECTED\n");
+            ESP_LOGI(TAG, "HTTP_EVENT_DISCONNECTED\n");
             break;
         default:
             break;
