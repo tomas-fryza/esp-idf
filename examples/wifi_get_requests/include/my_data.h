bb4d0b80b36c687b41e29b5feb247bd5c7b21705
Tomas Fryza <tomas.fryza@gmail.com>
2022-12-15 23:20:13 +0100

    removed duplicated function

--- examples/wifi_get_requests/include/my_data.h
+++ examples/wifi_get_requests/include/my_data.h
@@ -1,8 +1,8 @@
 #ifndef MY_DATA
 #define MY_DATA
 
-#define WIFI_SSID "YOUR_WIFI_SSID"
-#define WIFI_PASS "YOUR_WIFI_PASSWORD"
+// #define WIFI_SSID "YOUR_WIFI_SSID"
+// #define WIFI_PASS "YOUR_WIFI_PASSWORD"
 #define WIFI_MAXIMUM_RETRY  5
 
 #endif
